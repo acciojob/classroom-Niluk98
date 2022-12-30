0674c4547e9a397c18e75ae9f28e1f5bf361d4a6
niladri <niladriparamanik@gmail.com>
2022-12-30 12:30:29 +0530

    4

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("students")
+@RequestMapping("student")
 public class StudentController {
 // @Autowired
  StudentService studentService=new StudentService();
